d9efd6a9e5938f27dfce2c21b9584dab6f8c29bf
Dongho Kim <dhkima@higenmotor.com>
2023-04-27 14:34:18 +0900

    Debug : spaced path

--- git_operation.py
+++ git_operation.py
@@ -40,11 +40,12 @@ def git_clone(server,organ,repo,user,token,root,gitpath):
     else:
         try:
             print("\n# Clone starts from : \n"+URL_SOURCE)
-            os.system(gitpath+"\git.exe clone "+URL_SOURCE+" "+PATH_TARGET)
-            print(gitpath+"\git.exe clone "+URL_SOURCE2+" "+PATH_TARGET)
-            os.chdir("'"+PATH_TARGET+"'")
-            os.system(gitpath+"\git.exe lfs install --local")
-            print(gitpath+"\git.exe lfs install --local")
+            gitpath2 = '"'+gitpath+"\git.exe"+'"'
+            os.system(gitpath2+" clone "+URL_SOURCE+" "+PATH_TARGET)
+            print("\n"+gitpath2+" clone "+URL_SOURCE2+" "+PATH_TARGET)
+            os.chdir(PATH_TARGET)
+            os.system(gitpath2+" lfs install --local")
+            print("\n"+gitpath2+" lfs install --local")
             return 0
         except:
             print("\n# No Repo : "+URL_SOURCE2)
@@ -52,27 +53,31 @@ def git_clone(server,organ,repo,user,token,root,gitpath):
 
 def git_pull(repo,root,gitpath):
     PATH_TARGET = root+"\\"+repo
+    gitpath2 = '"'+gitpath+"\git.exe"+'"'
     if os.path.isdir(PATH_TARGET):
-        os.chdir(PATH_TARGET)
-        os.system(gitpath+"\git.exe fetch --all")
-        print(gitpath+"\git.exe fetch --all")
-        os.system(gitpath+"\git.exe pull --all")
-        print(gitpath+"\git.exe pull --all")
-        return 0
+        try:
+            os.chdir(PATH_TARGET)
+            os.system(gitpath2+" fetch --all")
+            print("\n"+gitpath2+" fetch --all")
+            os.system(gitpath2+" pull --all")
+            print("\n"+gitpath2+" pull --all")
+            return 0
+        except:
+            print("\n# No Repo : "+PATH_TARGET)
+            return -2
     else:
         print("\n# Not exists : \n"+PATH_TARGET)
         return -1
 
 def git_make(server,organ,repo,user,token,root,gitpath,exepath):
-    URL_SOURCE = "https://"+server+"/"+organ+"/"+repo+".git"
+    #URL_SOURCE = "https://"+server+"/"+organ+"/"+repo+".git"
     PATH_TARGET = root+"\\"+repo
+    gitpath2 = '"'+gitpath+"\git.exe"+'"'
     if os.path.isdir(PATH_TARGET):
         print("\n# Already exist : "+PATH_TARGET)
         return -1
     else:
         # Make new repository on Github.com
-        #os.system("gh repo create --public "+repo)
-        #print("gh repo create --public "+repo)
         os.system("gh repo create --internal "+organ+"/"+repo)
         print("gh repo create --internal "+organ+"/"+repo)
         # Clone
@@ -82,8 +87,8 @@ def git_make(server,organ,repo,user,token,root,gitpath,exepath):
         # Init
         if error==0:
             print("\n# Initializing... \n")
-            os.system(gitpath+"\git.exe lfs install --local")
-            print(gitpath+"\git.exe lfs install --local")
+            os.system(gitpath2+" lfs install --local")
+            print("\n"+gitpath2+" lfs install --local")
             os.system("copy "+exepath+"\\Dotfiles\\_gitattributes "+PATH_TARGET+"\\.gitattributes")
             print("\ncopy "+exepath+"\\Dotfiles\\_gitattributes "+PATH_TARGET+"\\.gitattributes")
             os.system("copy "+exepath+"\\Dotfiles\\_gitignore "+PATH_TARGET+"\\.gitignore")
@@ -95,12 +100,12 @@ def git_make(server,organ,repo,user,token,root,gitpath,exepath):
             os.system("echo "+"# "+repo+">"+PATH_TARGET+"\\3D\\README.md")
             print("\necho "+"# "+repo+">"+PATH_TARGET+"\\3D\\README.md")
             # 1st Push
-            os.system(gitpath+"\git.exe add --all")
-            print(gitpath+"\git.exe add --all")
-            os.system(gitpath+"\git.exe commit -m \"1st\"")
-            print(gitpath+"\git.exe commit -m \"1st\"")
-            os.system(gitpath+"\git.exe push --set-upstream origin HEAD")
-            print(gitpath+"\git.exe push --set-upstream origin HEAD")
+            os.system(gitpath2+" add --all")
+            print("\n"+gitpath2+" add --all")
+            os.system(gitpath2+" commit -m \"1st\"")
+            print("\n"+gitpath2+" commit -m \"1st\"")
+            os.system(gitpath2+" push --set-upstream origin HEAD")
+            print("\n"+gitpath2+" push --set-upstream origin HEAD")
             print("\nCheck [Archives]:[Include Git LFS objects in archives] in this address :\n")
             print("https://"+server+"/"+organ+"/"+repo+"/settings")
             return 0
@@ -110,19 +115,18 @@ def git_make(server,organ,repo,user,token,root,gitpath,exepath):
 
 def git_push(repo,root,gitpath,commit_message,change_branch):
     PATH_TARGET = root+"\\"+repo
+    gitpath2 = '"'+gitpath+"\git.exe"+'"'
     if os.path.isdir(PATH_TARGET):
         try:
             os.chdir(PATH_TARGET)
-            os.system(gitpath+"\git.exe checkout "+change_branch)
-            print(gitpath+"\git.exe checkout "+change_branch)
-            os.system(gitpath+"\git.exe add --all")
-            print(gitpath+"\git.exe add --all")
-            os.system(gitpath+"\git.exe commit -m \""+commit_message+"\"")
-            print(gitpath+"\git.exe commit -m \""+commit_message+"\"")
-            os.system(gitpath+"\git.exe push --all")
-            print(gitpath+"\git.exe push --all")
-            #os.system(gitpath+"\git.exe push -u origin "+change_branch)
-            #print(gitpath+"\git.exe push -u origin "+change_branch)
+            os.system(gitpath2+" checkout "+change_branch)
+            print("\n"+gitpath2+" checkout "+change_branch)
+            os.system(gitpath2+" add --all")
+            print("\n"+gitpath2+" add --all")
+            os.system(gitpath2+" commit -m \""+commit_message+"\"")
+            print("\n"+gitpath2+" commit -m \""+commit_message+"\"")
+            os.system(gitpath2+" push --all")
+            print("\n"+gitpath2+" push --all")
             print("\n# Push Finished. \n")
             return 0
         except:
@@ -132,15 +136,15 @@ def git_push(repo,root,gitpath,commit_message,change_branch):
         print("\n# Not exists : \n"+PATH_TARGET)
         return -1
 
-
 def git_branch(repo,root,gitpath,change_branch):
     PATH_TARGET = root+"\\"+repo
+    gitpath2 = '"'+gitpath+"\git.exe"+'"'
     if os.path.isdir(PATH_TARGET):
         os.chdir(PATH_TARGET)
-        os.system(gitpath+"\git.exe branch "+change_branch)
-        print(gitpath+"\git.exe branch "+change_branch)
-        os.system(gitpath+"\git.exe checkout "+change_branch)
-        print(gitpath+"\git.exe checkout "+change_branch)
+        os.system(gitpath2+" branch "+change_branch)
+        print("\n"+gitpath2+" branch "+change_branch)
+        os.system(gitpath2+" checkout "+change_branch)
+        print("\n"+gitpath2+" checkout "+change_branch)
         return 0
     else:
         print("# Not exists : \n"+PATH_TARGET)
@@ -148,12 +152,13 @@ def git_branch(repo,root,gitpath,change_branch):
 
 def git_merge(repo,root,gitpath,change_branch,change_merge):
     PATH_TARGET = root+"\\"+repo
+    gitpath2 = '"'+gitpath+"\git.exe"+'"'
     if os.path.isdir(PATH_TARGET):
         os.chdir(PATH_TARGET)
-        os.system(gitpath+"\git.exe checkout "+change_branch)
-        print(gitpath+"\git.exe checkout "+change_branch)
-        os.system(gitpath+"\git.exe merge -Xtheirs "+change_merge)
+        os.system(gitpath2+" checkout "+change_branch)
+        print("\n"+gitpath2+" checkout "+change_branch)
+        os.system(gitpath2+" merge -Xtheirs "+change_merge)
         return 0
     else:
-        print("# Not exists : \n"+PATH_TARGET)
+        print("\n# Not exists : \n"+PATH_TARGET)
         return -1
