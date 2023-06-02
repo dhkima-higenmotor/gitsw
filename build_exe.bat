d24529c8cce1be197dc6922133b804679097aaaf
dhkima-higenmotor <dhkima@higenmotor.com>
2023-06-02 10:17:25 +0900

    Modify about pyinstaller

--- build_exe.bat
+++ build_exe.bat
@@ -1,11 +1,15 @@
-pyinstaller --onefile --noconsole config.py
-COPY /Y .\dist\config.exe .\
-RMDIR /S /Q dist
-RMDIR /S /Q build
-DEL /Q config.spec
+REM pyinstaller --onefile --noconsole config.py
+pyinstaller --noconsole --upx-dir=%userprofile%\scoop\apps\upx\current config.py
 
-pyinstaller --onefile gitsw.py
-COPY /Y .\dist\gitsw.exe .\
-RMDIR /S /Q dist
-RMDIR /S /Q build
-DEL /Q gitsw.spec
+REM COPY /Y .\dist\config.exe .\
+REM RMDIR /S /Q dist
+REM RMDIR /S /Q build
+REM DEL /Q config.spec
+
+REM pyinstaller --onefile gitsw.py
+pyinstaller --upx-dir=%userprofile%\scoop\apps\upx\current gitsw.py
+
+REM COPY /Y .\dist\gitsw.exe .\
+REM RMDIR /S /Q dist
+REM RMDIR /S /Q build
+REM DEL /Q gitsw.spec
